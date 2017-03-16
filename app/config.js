7c5be884deaae900cc193d69e2972525b82cd95e
hughshen <hughssss@outlook.com>
2017-03-16 23:39:20 +0800

    cid改为由nginx生成

--- app/config.js
+++ app/config.js
@@ -80,24 +80,8 @@ var page = {
             }
         }
     }
-};
-// https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function uuid() {
-    if (!/cid=.*/.test(document.cookie)) {
-        var d = new Date().getTime();
-        var cid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d / 16);
-            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        });
-        var d = new Date();
-        d.setFullYear(d.getFullYear() + 2);
-        document.cookie = 'cid=' + cid + ';expires=' + d.toGMTString() + ';path=/;secure';
-    }
-    var res = document.cookie.match(/cid=[\w-]+;?/);
-    return res ? res[0].replace(';', '') : '';
 }
-;!function(a,b,c){var d=a.screen,e=encodeURIComponent,f=["dt="+e(b.title),"dr="+e(b.referrer),"ul="+(c.language||c.browserLanguage),"sd="+d.colorDepth+"-bit","sr="+d.width+"x"+d.height,"dl="+e(a.location.href),uuid()],g="?"+f.join("&");a.__ga_img=new Image,a.__ga_img.src="/ga.html"+g}(window,document,navigator,location);
+;!function(a,b,c){var d=a.screen,e=encodeURIComponent,f=["dt="+e(b.title),"dr="+e(b.referrer),"ul="+(c.language||c.browserLanguage),"sd="+d.colorDepth+"-bit","sr="+d.width+"x"+d.height,"dl="+e(a.location.href)],g="?"+f.join("&");a.__ga_img=new Image,a.__ga_img.src="/ga.html"+g}(window,document,navigator,location);
 </script>
 </body>
 </html>
